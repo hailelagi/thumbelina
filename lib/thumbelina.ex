2d3490f1e0eda38439db18c2ffd772fa2a0b78ef
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-05-21 18:06:51 +0100

    chore: specify lib behaviour

--- lib/thumbelina.ex
+++ lib/thumbelina.ex
@@ -2,17 +2,22 @@ defmodule Thumbelina do
   @moduledoc """
   Documentation for `Thumbelina`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Thumbelina.hello()
-      :world
-
-  """
   def hello do
     :world
+  end
+
+  # resize an image
+  def resize() do
+    nil
+  end
+
+  # change image format
+  def convert() do
+    nil
+  end
+
+  # save
+  def save() do
+    nil
   end
 end
