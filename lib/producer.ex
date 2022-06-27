999f433355d148dec7e4df10285f706bd67db9fe
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-06-27 12:52:04 +0100

    feat: scaffold pipleline

---
+++ lib/producer.ex
@@ -0,0 +1,12 @@
+defmodule Thumbelina.Producer do
+  use GenStage
+
+  def init(counter) do
+    {:producer, counter}
+  end
+
+  def handle_demand(demand, counter) do
+    events = Enum.to_list(counter..counter+demand-1)
+    {:noreply, events, counter + demand}
+  end
+end
--- lib/thumbelina.ex
+++ lib/thumbelina.ex
@@ -9,11 +9,16 @@ defmodule Thumbelina do
   - pause/resume processing saving memory
   - lazy and efficient runtime
   """
+  alias Thumbelina.{Consumer, Producer}
   use Rustler, otp_app: :thumbelina, crate: "thumbelina"
 
   # resize an image
   def resize() do
-    nil
+    # File.stream!(path, :line) |> Flow.from_enumerable()
+    {:ok, counter} = GenStage.start_link(Prod, nil)
+    {:ok, printer} = GenStage.start_link(Consumer, :ok)
+
+    GenStage.sync_subscribe(printer, to: counter)
   end
 
   # def batch_resize(path) do
@@ -22,9 +27,16 @@ defmodule Thumbelina do
 
   # change image format
   def convert() do
+    {:ok, counter} = GenStage.start_link(Producer, nil)
     nil
   end
 
+  ## GenStage
+  # elixir process that asks for data (message contract)
+  # back pressure to producer
+  # producer -> producer_consumer -> consumer
+  # [event, event, event]
+  # demand dispatcher
   # save
   def save() do
     nil
