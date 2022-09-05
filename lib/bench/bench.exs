a2ab0dd42dc666ad688e1240b38f5d13f838d23f
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-09-05 22:22:39 +0100

    chore: ci, config, bench boilerplate

---
+++ lib/bench/bench.exs
@@ -0,0 +1,11 @@
+list = Enum.to_list(1..10_000)
+map_fun = fn i -> [i, i * i] end
+
+Benchee.run(
+  %{
+    "flat_map" => fn -> Enum.flat_map(list, map_fun) end,
+    "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten() end
+  },
+  time: 10,
+  memory_time: 2
+)
