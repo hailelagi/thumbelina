c7587b19659b233818911a61cb6f172041f8ffb3
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-02-17 12:09:16 +0100

    config: define sup tree

---
+++ lib/thumbelina/application.ex
@@ -0,0 +1,12 @@
+defmodule Thumbelina.Application do
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = []
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
